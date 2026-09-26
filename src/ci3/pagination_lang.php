bf72c6ea14edef65d5be0c134cb9ec4da38989d8
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-26 10:24:09 +0100

    Squashing Commit!

---
+++ src/ci3/pagination_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['pagination_first_link'] = '&lsaquo; Erste';
+$lang['pagination_last_link'] = 'Letzte &rsaquo;';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_summary_text'] = 'Zeige %d bis %d von %d Einträgen';
---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - German
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => 'Deutsch',
+	'name_en'    => 'German',
+	'locale'     => 'de_DE',
+	'tag'        => 'de-DE',
+	'direction'  => 'ltr',
+	'code'       => 'de',
+	'flag'       => 'de',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => 'Arabisch',
+		'armenian'   => 'Armenisch',
+		'bengali'    => 'Bengalisch',
+		'brazilian'  => 'Portugiesisch (Brasilien)',
+		'bulgarian'  => 'Bulgarisch',
+		'catalan'    => 'Katalanisch',
+		'chinese'    => 'Chinesisch (Vereinfacht)',
+		'croatian'   => 'Kroatisch',
+		'czech'      => 'Tschechisch',
+		'danish'     => 'Dänisch',
+		'dutch'      => 'Niederländisch',
+		'english'    => 'Englisch',
+		'estonian'   => 'Estnisch',
+		'filipino'   => 'Filipino',
+		'finnish'    => 'Finnisch',
+		'french'     => 'Französisch',
+		'georgian'   => 'Georgisch',
+		'german'     => 'Deutsch',
+		'greek'      => 'Griechisch',
+		'hindi'      => 'Hindi',
+		'hungarian'  => 'Ungarisch',
+		'indonesian' => 'Indonesisch',
+		'italian'    => 'Italienisch',
+		'japanese'   => 'Japanisch',
+		'korean'     => 'Koreanisch',
+		'latvian'    => 'Lettisch',
+		'lithuanian' => 'Litauisch',
+		'malay'      => 'Malaiisch',
+		'norwegian'  => 'Norwegisch',
+		'persian'    => 'Persisch',
+		'polish'     => 'Polnisch',
+		'portuguese' => 'Portugiesisch',
+		'romanian'   => 'Rumänisch',
+		'russian'    => 'Russisch',
+		'serbian'    => 'Serbisch',
+		'slovak'     => 'Slowakisch',
+		'slovenian'  => 'Slowenisch',
+		'spanish'    => 'Spanisch',
+		'swahili'    => 'Suaheli',
+		'swedish'    => 'Schwedisch',
+		'taiwanese'  => 'Chinesisch (Traditionell)',
+		'tamil'      => 'Tamilisch',
+		'thai'       => 'Thailändisch',
+		'turkish'    => 'Türkisch',
+		'ukrainian'  => 'Ukrainisch',
+		'urdu'       => 'Urdu',
+		'vietnamese' => 'Vietnamesisch',
+	],
+];
